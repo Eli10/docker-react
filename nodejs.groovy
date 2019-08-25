f0a81596b9f8c1e734caef46ce852e6521699754
Elijah Augustin <elijahaugustin@yahoo.com>
2019-08-24 21:35:17 -0400

    Jenkins Job DSL file

---
+++ nodejs.groovy
@@ -0,0 +1,26 @@
+job('react-docker') {
+
+  scm {
+      git('git@github.com:Eli10/docker-react.git') { node ->
+
+        node / gitConfigName('DSL User')
+        node / gitConfigEmail('eaugustine@securityscorecard.io')
+
+      }
+  }
+
+  triggers {
+    scm('H/5 * * * *')
+  }
+
+  wrappers {
+    nodejs('nodejs') // name of the NodeJS installation in
+                    // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+  }
+
+  steps {
+    shell("npm install")
+  }
+
+
+}
